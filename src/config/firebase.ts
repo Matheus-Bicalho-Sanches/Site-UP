0237d5d6d3a0568f7f81b37ae59867cbd90a767b
Matheus-Bicalho-Sanches <matheussancheslondrina@gmail.com>
2025-02-03 14:57:33 -0300

    Deploy fix again

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase only if it hasn't been initialized already and we're on the client side
-let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
 
 export default app; 
