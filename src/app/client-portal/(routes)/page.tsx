720380ffbe6fa2c7eff83f1954612c4e4fad6c6d
Matheus-Bicalho-Sanches <matheussancheslondrina@gmail.com>
2025-02-21 07:02:16 -0300

    Fix 7

--- src/app/client-portal/(routes)/page.tsx
+++ src/app/client-portal/(routes)/page.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from 'next'
 import ClientPortalContent from './components/ClientPortalContent'
 
 // Marcar a página como dinâmica
@@ -7,12 +6,6 @@ export const dynamic = 'force-dynamic'
 // Opcional: Desabilitar pré-renderização estática
 export const generateStaticParams = () => {
   return []
-}
-
-// Metadados da página
-export const metadata: Metadata = {
-  title: 'Portal do Cliente',
-  description: 'Portal do cliente UP Gestão',
 }
 
 export default function ClientPortalPage() {
