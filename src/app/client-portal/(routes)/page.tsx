435d5002e4a476b8ea409e929caead4b88b52759
Matheus-Bicalho-Sanches <matheussancheslondrina@gmail.com>
2025-02-21 07:04:53 -0300

    Fix 8

--- src/app/client-portal/(routes)/page.tsx
+++ src/app/client-portal/(routes)/page.tsx
@@ -1,12 +1,6 @@
+'use client'
+
 import ClientPortalContent from './components/ClientPortalContent'
-
-// Marcar a página como dinâmica
-export const dynamic = 'force-dynamic'
-
-// Opcional: Desabilitar pré-renderização estática
-export const generateStaticParams = () => {
-  return []
-}
 
 export default function ClientPortalPage() {
   return <ClientPortalContent />
